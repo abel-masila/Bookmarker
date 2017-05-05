abc04871d20e06b2c9621c92ea887e031e34e4a7
Abel <abelmasila@gmail.com>
2017-05-05 14:47:34 +0300

    Finalized on fetchBookmarks() function and form validation

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ function saveBookmark(e){
 	//get form variables
 	var site_name=document.getElementById('site_name').value;
 	var site_url=document.getElementById('site_url').value;
+
+	if(!validateForm(site_name,site_url)){
+		return false;
+	}
 	var bookmark={
 		name:site_name,
 		url:site_url
@@ -33,6 +37,62 @@ function saveBookmark(e){
 		//Re-set it back to localStorage
 		localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
 	}
+
+	//Refetch bookmarks
+	fetchBookmarks();
 	//Prevent form from submiting
 	e.preventDefault();
+}
+
+//Delete bookmarks
+function deleteBookmark(url){
+	//Get bookmarks from localStorage
+	var bookmarks=JSON.parse(localStorage.getItem('bookmarks'));
+	for(var i=0;i<bookmarks.length;i++){
+		if(bookmarks[i].url==url){
+			//Remove from Array
+			bookmarks.splice(1,1)
+		}
+	}
+	localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+	//Refetch bookmarks
+	fetchBookmarks();
+}
+
+//Fetch Bookmarks
+function fetchBookmarks(){
+	//Get bookmarks from localStorage
+	var bookmarks=JSON.parse(localStorage.getItem('bookmarks'));
+	//Get output id
+	var bookmarkResults=document.getElementById('BookmarkResults');
+	//Build Output
+	bookmarkResults.innerHTML='';
+	for(var i=0;i<bookmarks.length;i++){
+		var name=bookmarks[i].name;
+		var url=bookmarks[i].url;
+
+		bookmarkResults.innerHTML+='<div class="well">'+
+									'<h3>' + name+ 
+									' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> '+
+									' <a class="btn btn-danger" onclick="deleteBookmark(\''+url+'\')"  href="#">Delete</a> '+
+									'</h3>'+
+									'</div';
+	}
+
+}
+
+function validateForm(){
+
+	if(!site_name || !site_url){
+		alert("Please fill in the form");
+		return false;
+	}
+	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+	var regex = new RegExp(expression);
+
+	if(!site_url.match(regex)){
+		alert("Invalid URL");
+		return false;
+	}
+	return true;
 }
